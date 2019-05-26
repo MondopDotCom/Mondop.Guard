ac35847f083d2537760d3b4cb3d083d8c79661ab
Marcel Vroone <mvroone@it-actief.nl>
2019-05-26 15:56:08 +0200

    Initial commit

---
+++ src/Mondop.Guard/Mondop.Guard.Tests/EnsureTests.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace Mondop.Guard.Tests
+{
+    [TestClass]
+    public class EnsureTests
+    {
+        [TestMethod]
+        public void Call_IsNotNull_WithValidArgument_Expect_AssignedResult()
+        {
+            var test = new StringBuilder();
+
+            var result = Ensure.IsNotNull(test, nameof(test));
+
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNull_With_NullValue_Expect_Exception()
+        {
+            StringBuilder test = null;
+            Action action = () => Ensure.IsNotNull(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrEmpty_With_Value_Expect_AssignedResult()
+        {
+            string test = "Test";
+            var result = Ensure.IsNotNullOrEmpty(test, nameof(test));
+
+            result.Should().Be("Test");
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrEmpty_With_NullValue_Expect_ArgumentNullException()
+        {
+            string test = null;
+            Action action = () => Ensure.IsNotNullOrEmpty(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrEmpty_With_NullValue_Expect_ArgumentException()
+        {
+            string test = "";
+            Action action = () => Ensure.IsNotNullOrEmpty(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrWhiteSpace_With_Value_Expect_AssignedResult()
+        {
+            string test = "Test";
+            var result = Ensure.IsNotNullOrWhiteSpace(test, nameof(test));
+
+            result.Should().Be("Test");
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrWhiteSpace_With_NullValue_Expect_ArgumentNullException()
+        {
+            string test = null;
+            Action action = () => Ensure.IsNotNullOrWhiteSpace(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrWhiteSpace_With_EmptyValue_Expect_ArgumentException()
+        {
+            string test = "";
+            Action action = () => Ensure.IsNotNullOrWhiteSpace(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+        [TestMethod]
+        public void Call_IsNotNullOrWhiteSpace_With_WhiteSpaceValue_Expect_ArgumentException()
+        {
+            string test = "\t ";
+            Action action = () => Ensure.IsNotNullOrWhiteSpace(test, nameof(test));
+
+            action.Should().ThrowExactly<ArgumentException>();
+        }
+
+    }
+}
---
+++ src/Mondop.Guard/Mondop.Guard/Ensure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Mondop.Guard
+{
+    public static class Ensure
+    {
+        public static TArg IsNotNull<TArg>(TArg argument, string name)
+        {
+            if (argument == null)
+                throw new ArgumentNullException(name);
+
+            return argument;
+        }
+
+        public static string IsNotNullOrEmpty(this string argument,string name)
+        {
+            if (argument == null)
+                throw new ArgumentNullException(name);
+
+            if (string.IsNullOrEmpty(argument))
+                throw new ArgumentException($"Empty string not allowed for {name}");
+
+            return argument;
+        }
+
+        public static string IsNotNullOrWhiteSpace(this string argument,string name)
+        {
+            if (argument == null)
+                throw new ArgumentNullException(name);
+
+            if (string.IsNullOrWhiteSpace(argument))
+                throw new ArgumentException($"Empty or white space only string not allowed for {name}");
+
+            return argument;
+        }
+    }
+}
